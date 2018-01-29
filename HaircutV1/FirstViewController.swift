f4549d07e33d384da95e7f4996f5da7ddf1f3ddc
kkuroda27 <kkuroda27@gmail.com>
2018-01-29 14:07:30 -0800

    Initial Commit

---
+++ HaircutV1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HaircutV1
+//
+//  Created by Kaito Kuroda on 1/29/18.
+//  Copyright © 2018 Kai Kuroda Company. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
