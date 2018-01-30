cca30d48da3450c0ef9d051d5e0dbc62bbb27720
kkuroda27 <kkuroda27@gmail.com>
2018-01-30 10:30:08 -0800

    First commit! So far I have two pages tabbed - View Haircuts + Create New Haircut. I created all UI elements for "Create New Haircut" Page and implemented functionality for multiple image uploads

--- HaircutV1/AppDelegate.swift
+++ HaircutV1/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "214ad04b2f476d6e1b9d132b477469c85c9910b3"
+            $0.clientKey = "9a2d29a80b4b97abf44d98ec4fb848261752bf47"
+            $0.server = "http://ec2-54-196-178-16.compute-1.amazonaws.com/parse"
+        }
+        Parse.initialize(with: configuration)
+
         return true
     }
 
@@ -39,8 +48,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
     }
-
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "HaircutV1")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 
 }
 
--- HaircutV1/SecondViewController.swift
+++ HaircutV1/SecondViewController.swift
@@ -8,8 +8,45 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet var imgLeft: UIImageView!
+    @IBOutlet var imgCenter: UIImageView!
+    @IBOutlet var imgRight: UIImageView!
+    
+    var imagePicked = 1
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print(imagePicked)
+            switch imagePicked {
+            case 2:
+                imgCenter.image = image
+            case 3:
+                imgRight.image = image
+            default:
+                imgLeft.image = image
+            }
+
+        } else {
+            print("There was a problem getting the image")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func chooseImgLeft(_ sender: UIButton) {
+        print(sender.tag)
+        imagePicked = sender.tag
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.allowsEditing = false
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
