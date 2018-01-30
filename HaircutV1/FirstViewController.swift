05c3ae0f19a1d664e2315ff779b3df55d78ac334
kkuroda27 <kkuroda27@gmail.com>
2018-01-30 10:30:59 -0800

    Committing FirstViewController.swift file for the first time.

--- HaircutV1/FirstViewController.swift
+++ HaircutV1/FirstViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import Parse
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        var gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground {
+            (success: Bool, error: Error?) in
+            if (success) {
+                print("Success!")
+            } else {
+                print("Failed!")
+            }
+        }
+        */
     }
 
     override func didReceiveMemoryWarning() {
