a40ab6b1ca4488b0fc8167c6e6a9e24580cb6e8e
kkuroda27 <kkuroda27@gmail.com>
2018-04-07 15:37:49 -0700

    Worked out a template launch screen for now.

---
+++ HaircutV1/LaunchScreenViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LaunchScreenViewController.swift
+//  HaircutV1
+//
+//  Created by Kaito Kuroda on 4/7/18.
+//  Copyright © 2018 Kai Kuroda Company. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    @IBOutlet var launchScreenAppIcon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
