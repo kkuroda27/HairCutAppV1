517ba7f65b8311342c76ea02afba39a8c11721ee
kkuroda27 <kkuroda27@gmail.com>
2018-05-02 10:25:08 -0700

    Had to implement this a bit oddly, but made sure the HELP popup textview starts at the top of the scrollview, plus flash scrollbars when it loads.

--- HaircutV1/HelpViewController.swift
+++ HaircutV1/HelpViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HelpViewController: UIViewController {
 
+    @IBOutlet var helpTextView: UITextView!
     @IBOutlet var popupView: UIView!
     @IBAction func closePopup(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -20,6 +21,18 @@ class HelpViewController: UIViewController {
         popupView.layer.cornerRadius = 10
         // necessary for image. //popupView.layer.masksToBounds = true
         
+    }
+    
+    // for some reason, to ensure the text starts at the top of the scrollview, I had to disable and enable isScrollEnabled as in the below two functions.
+    override func viewWillAppear(_ animated: Bool) {
+        self.helpTextView.isScrollEnabled = false
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print("FUNCTION START: viewDidAppear - HelpViewController.swift")
+        self.helpTextView.flashScrollIndicators()
+        self.helpTextView.isScrollEnabled = true
+
     }
 
 }
